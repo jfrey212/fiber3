b00fb49983d4d99d7066d1883a84e67973ebdaed
jfrey212 <eojyerf@gmail.com>
2021-08-19 09:35:57 -0500

    Initial commit, basic requirements listed

---
+++ fiber3.rb
@@ -0,0 +1,24 @@
+#3rd version of air sample entry program
+
+#Sample class definition - a sample has count, volume, duration, fields, id
+#Potentially use time objects to dynamically generate the duration
+#Samples are stored in a hash - the sample ID will be the hash key to access the sample
+
+#CRUD actions for fiber objects - create, read, update, delete
+#Are these class methods?
+
+#Locally stored sample variables can be written into a file
+#Files can be loaded into the program for displaying past data
+#program checks the local directory for sample files
+#Display available files in a list, user selects the file
+#option to look in a different directory?
+
+#use regex to check user input
+
+#Main program loop - user selects option, etc.
+
+#Commands to break out of the loop - quit with save, quit without saving
+
+#Is it possible to use Prawn gem to generate a PDF air sample log - may only be possible in Rails
+
+#Use Git this time for version control
